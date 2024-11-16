fb862a017b86955d3c6f4d633484a4ca787b5cf3
bpandey3 <bpandey3@hotmail.com>
2024-11-15 20:10:21 -0500

    update linkedin scrapper

--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -10,20 +10,20 @@ def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
     Manually scrape the information from the LinkedIn profile"""
 
     # if mock:
-    linkedin_profile_url = "https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/78233eb934aa9850b689471a604465b188e761a0/eden-marco.json"
+    #     linkedin_profile_url = "https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/78233eb934aa9850b689471a604465b188e761a0/eden-marco.json"
+    #     response = requests.get(
+    #         linkedin_profile_url,
+    #         timeout=10,
+    #     )
+    # else:
+    api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+    header_dic = {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
     response = requests.get(
-        linkedin_profile_url,
+        api_endpoint,
+        params={"url": linkedin_profile_url},
+        headers=header_dic,
         timeout=10,
     )
-    # else:
-    #     api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
-    #     header_dic = {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
-    #     response = requests.get(
-    #         api_endpoint,
-    #         params={"url": linkedin_profile_url},
-    #         headers=header_dic,
-    #         timeout=10,
-    #    )
 
     data = response.json()
     data = {
