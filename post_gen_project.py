3a4775dc7492a5564ef8fe1cb07619e7d9d0ae80
BP <bpandey3@hotmail.com>
2025-07-16 23:08:04 -0400

    Update post_gen_project.py

--- post_gen_project.py
+++ post_gen_project.py
@@ -1,17 +1,26 @@
 import os
 import shutil
+import json
 
 def move_lambda_to_lambdas():
-    lambda_name = os.environ["COOKIECUTTER_lambda_name"]
+    # Read cookiecutter context from cookiecutter.json in current directory
+    context_file = os.path.join(os.getcwd(), 'cookiecutter.json')
+    with open(context_file, 'r') as f:
+        context = json.load(f)
+
+    lambda_name = context['lambda_name']
     project_dir = os.getcwd()
     src_path = os.path.join(project_dir, lambda_name)
-    dest_dir = os.path.join(project_dir, "lambdas")
+    dest_dir = os.path.join(project_dir, 'lambdas')
     dest_path = os.path.join(dest_dir, lambda_name)
 
     os.makedirs(dest_dir, exist_ok=True)
 
-    # Move the folder into lambdas/
+    # Move lambda folder into lambdas/
     shutil.move(src_path, dest_path)
+
+    # Optional: remove cookiecutter.json if you don’t want it left behind
+    os.remove(context_file)
 
 if __name__ == "__main__":
     move_lambda_to_lambdas()
