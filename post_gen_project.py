ebf48f639c5f731431ec386b325b4a579ec93cb3
BP <bpandey3@hotmail.com>
2025-07-16 22:45:45 -0400

    Update post_gen_project.py

--- post_gen_project.py
+++ post_gen_project.py
@@ -1,4 +1,11 @@
 import os
+import shutil
 
-lambda_path = os.path.join("lambdas", "{{ cookiecutter.lambda_name }}")
-os.makedirs(lambda_path, exist_ok=True)
+lambda_name = os.environ['COOKIECUTTER_lambda_name']
+base_dir = os.path.abspath(os.path.join(os.getcwd(), '..', '..', 'genai-lambda-base', 'lambdas'))
+target_path = os.path.join(base_dir, lambda_name)
+
+os.makedirs(target_path, exist_ok=True)
+
+# copy rendered files from current dir to the target folder
+shutil.copyfile('index.py', os.path.join(target_path, 'index.py'))
